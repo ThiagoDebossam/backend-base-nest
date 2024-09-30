ad43b7eecba7197b42aabf6d77975366034cf255
Thiago Nogueira <thiago.nogueira@a4pm.com.br>
2024-09-30 20:53:42 -0300

    Integration tests - validate email field on create user

--- src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
@@ -25,8 +25,41 @@ describe('UserEntity integration tests', () => {
                 name: 'a'.repeat(256)
             }
             expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+
+            props = {
+                ...UserDataBuilder({}),
+                name: 10 as any
+            }
+            expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
         })
 
+
+        it ('Should throw an error when creating an user with invalid email', () => {
+
+            let props: UserProps = {
+                ...UserDataBuilder({}),
+                email: null
+            }
+            expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+
+            props = {
+                ...UserDataBuilder({}),
+                email: ''
+            }
+            expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+
+            props = {
+                ...UserDataBuilder({}),
+                email: 'a'.repeat(256)
+            }
+            expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+
+            props = {
+                ...UserDataBuilder({}),
+                email: 10 as any
+            }
+            expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+        })
     })
 
 })
