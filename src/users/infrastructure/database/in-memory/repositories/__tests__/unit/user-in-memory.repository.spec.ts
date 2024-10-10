ad0247c16eed6c40a0d1049175c7d3d72811572e
Thiago Nogueira <thiago.nogueira@a4pm.com.br>
2024-10-09 22:10:57 -0300

    apllysort test

--- src/users/infrastructure/database/in-memory/repositories/__tests__/unit/user-in-memory.repository.spec.ts
+++ src/users/infrastructure/database/in-memory/repositories/__tests__/unit/user-in-memory.repository.spec.ts
@@ -60,4 +60,29 @@ describe ('UserInMemoryRepository unit tests', () => {
             items[0], items[1]
         ])
     })
+
+    it('Should sort by createdat when sort param is null', async () => {
+        const createdAt = new Date()
+        const items = [
+            new UserEntity(UserDataBuilder({name: 'Test', createdAt})),
+            new UserEntity(UserDataBuilder({name: 'TEST', createdAt: new Date(createdAt.getTime() + 1)})),
+            new UserEntity(UserDataBuilder({name: 'fake', createdAt: new Date(createdAt.getTime() + 2)})),
+        ]
+        const itemsSorted = await sut['applySort'](items, null, null)
+        expect(itemsSorted).toStrictEqual([
+            items[2], items[1], items[0]
+        ])
+    })
+
+    it('Should sort by name', async () => {
+        const items = [
+            new UserEntity(UserDataBuilder({name: 'b'})),
+            new UserEntity(UserDataBuilder({name: 'a'})),
+            new UserEntity(UserDataBuilder({name: 'c'})),
+        ]
+        const itemsSorted = await sut['applySort'](items, 'name', 'asc')
+        expect(itemsSorted).toStrictEqual([
+            items[1], items[0], items[2]
+        ])
+    })
 })
