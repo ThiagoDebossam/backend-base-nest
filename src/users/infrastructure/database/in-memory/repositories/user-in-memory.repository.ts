2b269273081a252d4179e0d699bd5b1a8b5da905
Thiago Nogueira <thiago.nogueira@a4pm.com.br>
2024-10-09 21:46:01 -0300

    user search

--- src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -1,10 +1,13 @@
 import { ConflictError } from "@/shared/domain/errors/conflict-error"
 import { NotFoundError } from "@/shared/domain/errors/not-found-error"
 import { InMemorySearchableRepository } from "@/shared/domain/repositories/in-memory-searchable.repository"
+import { SortDirection } from "@/shared/domain/repositories/searchable-repository-contracts "
 import { UserEntity } from "@/users/domain/entities/user.entity"
 import { UserRepository } from "@/users/domain/repositories/user.repository"
 
-export class UserInMemoryRepository extends InMemorySearchableRepository<UserEntity> implements UserRepository {
+export class UserInMemoryRepository extends InMemorySearchableRepository<UserEntity> implements UserRepository.Repository {
+
+    sortableFields: string[] = ['name', 'createdAt']
 
     async findByEmail(email: string): Promise<UserEntity> {
         const entity = this.items.find(f => f.email === email)
@@ -21,8 +24,17 @@ export class UserInMemoryRepository extends InMemorySearchableRepository<UserEnt
         }
     }
 
-    protected async applyFilter(items: UserEntity[], filter: string | null): Promise<UserEntity[]> {
-        throw new Error('Fazer')
+    protected async applyFilter(items: UserEntity[], filter: UserRepository.Filter): Promise<UserEntity[]> {
+        if (!filter) return items
+        return items.filter(item => {
+            return item.props.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+        })
+    }
+
+    protected async applySort(items: UserEntity[], sort: string | null, sortDir: SortDirection | null): Promise<UserEntity[]> {
+        return !sort
+        ? super.applySort(items, 'createdAt', 'desc')
+        : super.applySort(items, sort, sortDir)
     }
 
 }
