1403a9333692700675384385bd1f2baa639d73cd
Thiago Nogueira <thiago.nogueira@a4pm.com.br>
2024-10-10 18:55:21 -0300

    Refactoring SignupUseCase

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -1,6 +1,7 @@
 import { UserRepository } from "@/users/domain/repositories/user.repository"
 import { BadRequestError } from "../errors/bad-request-error"
 import { UserEntity } from "@/users/domain/entities/user.entity"
+import { HashProvider } from "@/shared/application/providers/hash-provider"
 
 export namespace SignupUsecase{
     export type Input = {
@@ -19,7 +20,10 @@ export namespace SignupUsecase{
 
     export class Usecase {
 
-        constructor(private userRepository: UserRepository.Repository) {}
+        constructor(
+            private userRepository: UserRepository.Repository,
+            private hashProvider: HashProvider
+        ) {}
 
         async execute (input: Input): Promise<Output> {
             const {email, name, password} = input
@@ -28,7 +32,13 @@ export namespace SignupUsecase{
             }
             await this.userRepository.emailExists(email)
 
-            const entity = new UserEntity(input)
+            const hashPassword = await this.hashProvider.generateHash(password)
+
+            const entity = new UserEntity(
+                Object.assign(input, {
+                    password: hashPassword
+                })
+            )
             await this.userRepository.insert(entity)
 
             return entity.toJSON()
