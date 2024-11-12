190e73a02999d84f6cefcb8e0dd26f706ac98b94
Thiago Nogueira <thiago.nogueira@a4pm.com.br>
2024-11-11 21:10:01 -0300

    int tests primsa user

--- src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int-spec.ts
+++ src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int-spec.ts
@@ -6,6 +6,8 @@ import { DatabaseModule } from "@/shared/infrastructure/database/database.module
 import { NotFoundError } from "@/shared/domain/errors/not-found-error"
 import { UserEntity } from "@/users/domain/entities/user.entity"
 import { UserDataBuilder } from "@/users/domain/testing/helpers/user-data-builder"
+import { UserRepository } from "@/users/domain/repositories/user.repository"
+import { ConflictError } from "@/shared/domain/errors/conflict-error"
 
 describe('UserPrismaRepository integrations tests', () => {
     const prismaService = new PrismaClient()
@@ -25,7 +27,7 @@ describe('UserPrismaRepository integrations tests', () => {
     })
 
     it('should throws error when entity not found', async () => {
-        expect(() => sut.findById('fake')).rejects.toThrow(new NotFoundError('UserModel not found using ID fake'))
+        await expect(() => sut.findById('fake')).rejects.toThrow(new NotFoundError('UserModel not found using ID fake'))
     })
 
     it('should finds a entity by id', async () => {
@@ -58,5 +60,144 @@ describe('UserPrismaRepository integrations tests', () => {
         expect(entities).toHaveLength(1)
         expect(JSON.stringify(entities)).toBe(JSON.stringify([entity]))
         entities.map(item => expect(item.toJSON()).toStrictEqual(entity.toJSON()))
+    })
+
+    it('should throws error on update when entity not found', async () => {
+        const entity = new UserEntity(UserDataBuilder({}))
+        await expect(() => sut.update(entity)).rejects.toThrow(new NotFoundError(`UserModel not found using ID ${entity._id}`))
+    })
+
+    it('should update a entity', async () => {
+        const entity = new UserEntity(UserDataBuilder({}))
+        const newUser = await prismaService.user.create({
+            data: entity.toJSON()
+        })
+        entity.update('new name')
+        await sut.update(entity)
+        const output = await prismaService.user.findUnique({
+            where: {
+                id: entity._id
+            }
+        })
+        expect(output.name).toBe('new name')
+    })
+
+    it('should throws error on delete when entity not found', async () => {
+        const entity = new UserEntity(UserDataBuilder({}))
+        await expect(() => sut.delete(entity._id)).rejects.toThrow(new NotFoundError(`UserModel not found using ID ${entity._id}`))
+    })
+
+    it('should delete a entity', async () => {
+        const entity = new UserEntity(UserDataBuilder({}))
+        const newUser = await prismaService.user.create({
+            data: entity.toJSON()
+        })
+        await sut.delete(entity._id)
+        const output = await prismaService.user.findUnique({
+            where: {
+                id: entity._id
+            }
+        })
+        expect(output).toBeNull()
+    })
+
+    it('should throws error when entity not found', async () => {
+        await expect(() => sut.findByEmail('fake')).rejects.toThrow(new NotFoundError(`UserModel not found using email fake`))
+    })
+
+    it('should finds a entity by email', async () => {
+        const entity = new UserEntity(UserDataBuilder({}))
+        const newUser = await prismaService.user.create({
+            data: entity.toJSON()
+        })
+        const output = await sut.findByEmail(entity.email)
+        expect(output.toJSON()).toStrictEqual(entity.toJSON())
+    })
+
+    it('should throws error when entity found by email', async () => {
+        const entity = new UserEntity(UserDataBuilder({ email: 'a@a.com' }))
+        const newUser = await prismaService.user.create({
+            data: entity.toJSON()
+        })
+        await expect(() => sut.emailExists('a@a.com')).rejects.toThrow(new ConflictError(`E-mail address already used`))
+    })
+
+    it('should not finds a entity by email', async () => {
+        expect.assertions(0)
+        await sut.emailExists('a@a.com')
+    })
+
+    describe('Search method tests', () => {
+        it('should apply only pagination when the order params are null', async () => {
+            const createdAt = new Date()
+            const entities: UserEntity[] = []
+            const arrange = Array(16).fill(UserDataBuilder({}))
+            arrange.forEach((item, index) => {
+                entities.push(
+                    new UserEntity({
+                        ...item,
+                        email: `teste${index}@mail.com`,
+                        createdAt: new Date(createdAt.getTime() + index),
+                    })
+                )
+            })
+
+            await prismaService.user.createMany({
+                data: entities.map(item => item.toJSON())
+            })
+
+            const searchOutput = await sut.search(new UserRepository.SearchParams())
+            const items = searchOutput.items
+
+            expect(searchOutput).toBeInstanceOf(UserRepository.SearchResult)
+            expect(searchOutput.total).toBe(16)
+            expect(searchOutput.items.length).toBe(15)
+            searchOutput.items.forEach(item => {
+                expect(item).toBeInstanceOf(UserEntity)
+            })
+            items.reverse().forEach((item,index) => {
+                expect(`teste${index + 1}@mail.com`).toBe(item.email)
+            })
+        })
+
+
+        it('should search using filter, sort, and paginate', async () => {
+            const createdAt = new Date()
+            const entities: UserEntity[] = []
+            const arrange = ['test', 'a', 'TESTE', 'b', 'TeSt']
+            arrange.forEach((item, index) => {
+                entities.push(
+                    new UserEntity({
+                        ...UserDataBuilder({ name: item }),
+                        createdAt: new Date(createdAt.getTime() + index),
+                    })
+                )
+            })
+
+            await prismaService.user.createMany({
+                data: entities.map(item => item.toJSON())
+            })
+
+            const searchOutputPageOne = await sut.search(new UserRepository.SearchParams({
+                page: 1,
+                perPage: 2,
+                sort: 'name',
+                sortDir: 'asc',
+                filter: 'TEST'
+            }))
+
+            expect(searchOutputPageOne.items[0].toJSON()).toMatchObject(entities[0].toJSON())
+            expect(searchOutputPageOne.items[1].toJSON()).toMatchObject(entities[4].toJSON())
+
+            const searchOutputPageTwo = await sut.search(new UserRepository.SearchParams({
+                page: 2,
+                perPage: 2,
+                sort: 'name',
+                sortDir: 'asc',
+                filter: 'TEST'
+            }))
+
+            expect(searchOutputPageTwo.items[0].toJSON()).toMatchObject(entities[2].toJSON())
+        })
     })
 })
--- src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
+++ src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
@@ -3,22 +3,76 @@ import { PrismaService } from "@/shared/infrastructure/database/prisma/prisma.se
 import { UserEntity } from "@/users/domain/entities/user.entity"
 import { UserRepository } from "@/users/domain/repositories/user.repository"
 import { UserModelMapper } from "../models/user-model.mapper"
+import { ConflictError } from "@/shared/domain/errors/conflict-error"
 
 export class UserPrismaRepository implements UserRepository.Repository {
-    sortableFields: string[]
+    sortableFields = [
+        'name',
+        'createdAt'
+    ]
 
     constructor (private prismaService: PrismaService) {}
 
-    findByEmail(email: string): Promise<UserEntity> {
-        throw new Error("Method not implemented.")
+    async findByEmail(email: string): Promise<UserEntity> {
+        try {
+            const user = await this.prismaService.user.findUnique({
+                where: { email }
+            })
+            return UserModelMapper.toEntity(user)
+        } catch {
+            throw new NotFoundError(`UserModel not found using email ${email}`)
+        }
     }
 
-    emailExists(email: string): Promise<void> {
-        throw new Error("Method not implemented.")
+    async emailExists(email: string): Promise<void> {
+        const user = await this.prismaService.user.findUnique({
+            where: { email }
+        })
+        if (user) {
+            throw new ConflictError(`E-mail address already used`)
+        }
     }
 
-    search(props: UserRepository.SearchParams): Promise<UserRepository.SearchResult> {
-        throw new Error("Method not implemented.")
+    async search(props: UserRepository.SearchParams): Promise<UserRepository.SearchResult> {
+        const sortable = this.sortableFields?.includes(props.sort) || false
+        const orderByField = sortable ? props.sort : 'createdAt'
+        const orderByDir = sortable ? props.sortDir : 'desc'
+
+        const count = await this.prismaService.user.count({
+            ...(props.filter && {
+                where: {
+                    name: {
+                        contains: props.filter,
+                        mode: 'insensitive'
+                    }
+                }
+            })
+        })
+
+        const models = await this.prismaService.user.findMany({
+            ...(props.filter && {
+                where: {
+                    name: {
+                        contains: props.filter,
+                        mode: 'insensitive'
+                    }
+                }
+            }),
+            orderBy: {
+                [orderByField]: orderByDir
+            },
+            skip: props.page && props.page > 0 ? (props.page -1) * props.perPage : 1,
+            take: props.perPage && props.perPage > 0 ? props.perPage : 15
+        })
+        return new UserRepository.SearchResult({
+            items: models.map(model => UserModelMapper.toEntity(model)),
+            total: count,
+            currentPage: props.page,
+            perPage: props.perPage,
+            sort: orderByField,
+            sortDir: orderByDir,
+            filter: props.filter
+        })
     }
 
     async insert(entity: UserEntity): Promise<void> {
@@ -36,12 +90,21 @@ export class UserPrismaRepository implements UserRepository.Repository {
         return models.map(model => UserModelMapper.toEntity(model))
     }
 
-    update(entity: UserEntity): Promise<void> {
-        throw new Error("Method not implemented.")
+    async update(entity: UserEntity): Promise<void> {
+        await this._get(entity._id)
+        await this.prismaService.user.update({
+            data: entity.toJSON(),
+            where: {
+                id: entity._id
+            }
+        })
     }
 
-    delete(id: string): Promise<void> {
-        throw new Error("Method not implemented.")
+    async delete(id: string): Promise<void> {
+        await this._get(id)
+        await this.prismaService.user.delete({
+            where: { id }
+        })
     }
 
     protected async _get(id: string): Promise<UserEntity> {
