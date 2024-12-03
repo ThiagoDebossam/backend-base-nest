6dd7234050de68491a6b46659d4938186be3c5fc
Thiago Nogueira <thiago.nogueira@a4pm.com.br>
2024-12-03 19:11:47 -0300

    authguards

--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -10,6 +10,7 @@ import {
     HttpCode,
     Query,
     Put,
+    UseGuards,
 } from '@nestjs/common'
 import { SignUpDto } from './dtos/sign-up.dto'
 import { UpdateUserDto } from './dtos/update-user.dto'
@@ -26,6 +27,7 @@ import { UpdatePasswordDto } from './dtos/update-password.dto'
 import { UserOutput } from '../application/dtos/user-output'
 import { UserCollectionPresenter, UserPresenter } from './presenters/user.presenter'
 import { AuthService } from '@/auth/infrastructure/auth.service'
+import { AuthGuard } from '@/auth/infrastructure/auth.guard'
 
 @Controller('users')
 export class UsersController {
@@ -74,30 +76,35 @@ export class UsersController {
         return this.authService.generateJwt(output.id)
     }
 
+    @UseGuards(AuthGuard)
     @Get()
     async search(@Query() searchParams: ListUsersDto) {
         const output = await this.listUsersUseCase.execute(searchParams)
         return UsersController.listUsersToResponse(output)
     }
 
+    @UseGuards(AuthGuard)
     @Get(':id')
     async findOne(@Param('id') id: string) {
         const output = await this.getUserUseCase.execute({ id })
         return UsersController.userToResponse(output)
     }
 
+    @UseGuards(AuthGuard)
     @Put(':id')
     async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
         const output = await this.updateUserUseCase.execute({ id, ...updateUserDto })
         return UsersController.userToResponse(output)
     }
 
+    @UseGuards(AuthGuard)
     @Patch(':id')
     async updatePassword(@Param('id') id: string, @Body() updatePasswordDto: UpdatePasswordDto) {
         const output = await this.updatePasswordUseCase.execute({ id, ...updatePasswordDto })
         return UsersController.userToResponse(output)
     }
 
+    @UseGuards(AuthGuard)
     @HttpCode(204)
     @Delete(':id')
     async remove(@Param('id') id: string) {
